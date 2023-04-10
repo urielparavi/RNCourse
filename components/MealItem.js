7686b1afd9d228321ee071267494a911f1d1f6d9
Your Name <urielparavi75@gmail.com>
2023-04-11 00:03:54 +0300

    97-99

---
+++ components/MealItem.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+const MealItem = ({ title }) => {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  );
+};
+export default MealItem;
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, FlatList, View, Text } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const MealsOverviewScreen = ({ route }) => {
   // We extract the params object from navigate
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    // If we have a index greater or equal than zero, a category is part of that meal, because if not find a given
+    // category id in that array, indexOf would return minus one
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title} />;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
