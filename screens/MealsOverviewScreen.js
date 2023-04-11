0acb4d6f4dafea5f4d8a45eed56fe0a766feb0fd
Your Name <urielparavi75@gmail.com>
2023-04-11 19:24:32 +0300

    102

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,8 +1,9 @@
-import { StyleSheet, FlatList, View, Text } from 'react-native';
-import { MEALS } from '../data/dummy-data';
+import { useLayoutEffect } from 'react';
+import { StyleSheet, FlatList, View } from 'react-native';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 import MealItem from '../components/MealItem';
 
-const MealsOverviewScreen = ({ route }) => {
+const MealsOverviewScreen = ({ route, navigation }) => {
   // We extract the params object from navigate
   const catId = route.params.categoryId;
 
@@ -11,6 +12,16 @@ const MealsOverviewScreen = ({ route }) => {
     // category id in that array, indexOf would return minus one
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
 
   const renderMealItem = (itemData) => {
     const item = itemData.item;
