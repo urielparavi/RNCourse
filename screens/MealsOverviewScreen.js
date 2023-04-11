9c467d57a6dc0cbabf0313e0cafe344b572b0c3c
Your Name <urielparavi75@gmail.com>
2023-04-11 15:52:43 +0300

    100

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -13,7 +13,17 @@ const MealsOverviewScreen = ({ route }) => {
   });
 
   const renderMealItem = (itemData) => {
-    return <MealItem title={itemData.item.title} />;
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+
+    return <MealItem {...mealItemProps} />;
   };
 
   return (
