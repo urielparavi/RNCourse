20a229f626ee366aa1dc3d494c83847deba5a07e
Your Name <urielparavi75@gmail.com>
2023-04-11 21:50:18 +0300

    104

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,33 @@
-import { Text } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import MealDetails from '../components/MealDetails';
 
 const MealDetailScreen = ({ route }) => {
   // On that params object I have mealId property, because when navigating to this MealDetail screen,
   // I set the mealId on that params object
   const mealId = route.params.mealId;
 
-  return <Text>This is the Meal Detail screen ({mealId})</Text>;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  return (
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
+
+      <Text>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <Text key={step}>{step}</Text>
+      ))}
+    </View>
+  );
 };
 export default MealDetailScreen;
