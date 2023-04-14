3d28ca79260ccfa81e380dd027f96ee5361b00a2
Your Name <urielparavi75@gmail.com>
2023-04-14 20:08:47 +0300

    128

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,6 @@
+import { FlatList } from 'react-native';
+
+const ExpensesList = () => {
+  return <FlatList />;
+};
+export default ExpensesList;
---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+import ExpensesSummary from './ExpensesSummary';
+import ExpensesList from './ExpensesList';
+
+const ExpensesOutput = ({ expenses }) => {
+  return (
+    <View>
+      <ExpensesSummary />
+      <ExpensesList />
+    </View>
+  );
+};
+export default ExpensesOutput;
