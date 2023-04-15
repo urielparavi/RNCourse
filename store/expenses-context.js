91f5c1e6500b6fcaac0a4d24bc64e1eded8f01c3
Your Name <urielparavi75@gmail.com>
2023-04-15 21:40:53 +0300

    142

---
+++ store/expenses-context.js
@@ -0,0 +1,112 @@
+import { createContext, useReducer } from 'react';
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    description: 'A pair of shoes',
+    amount: 59.99,
+    date: new Date('2021-12-19'),
+  },
+  {
+    id: 'e2',
+    description: 'A pair of trousers',
+    amount: 89.29,
+    date: new Date('2021-01-05'),
+  },
+  {
+    id: 'e3',
+    description: 'Some bananas',
+    amount: 5.99,
+    date: new Date('2021-12-01'),
+  },
+  {
+    id: 'e4',
+    description: 'A book',
+    amount: 14.99,
+    date: new Date('2022-02-19'),
+  },
+  {
+    id: 'e5',
+    description: 'Another book',
+    amount: 18.99,
+    date: new Date('2022-02-18'),
+  },
+  {
+    id: 'e6',
+    description: 'A pair of trousers',
+    amount: 89.29,
+    date: new Date('2021-01-05'),
+  },
+  {
+    id: 'e7',
+    description: 'Some bananas',
+    amount: 5.99,
+    date: new Date('2021-12-01'),
+  },
+  {
+    id: 'e8',
+    description: 'A book',
+    amount: 14.99,
+    date: new Date('2022-02-19'),
+  },
+  {
+    id: 'e9',
+    description: 'Another book',
+    amount: 18.99,
+    date: new Date('2022-02-18'),
+  },
+];
+
+export const ExpensesContext = createContext({
+  expenses: [],
+  addExpense: ({ description, amount, date }) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amount, date }) => {},
+});
+
+const expensesReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      const id = new Date().toString() + Math.random().toString();
+      return [{ ...action.payload, id: id }, ...state];
+    case 'UPDATE':
+      const updatableExpenseIndex = state.findIndex(
+        (expense) => expense.id === action.payload.id
+      );
+      const updatableExpense = state[updatableExpenseIndex];
+      // We merge the update description, amount and date into this object with (action.payload.data) and we will therefore
+      // override the existing values for description, amount and date, but we will keep the original id though
+      const updatedItem = { ...updatableExpense, ...action.payload.data };
+      // The overall array that should be updated and construct a new arrya of cours
+      const updatedExpenses = [...state];
+      // We override the item at this index
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
+      // The new array based on the old array with one item updated
+      return updatedExpenses;
+    case 'DELETE':
+      return state.filter((expense) => expense.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+const ExpensesContextProvider = ({ children }) => {
+  // DUMMY_EXPENSES => will be used as an initial value before the reducer executed the first time
+  const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+
+  const addExpense = (expenseData) => {
+    dispatch({ type: 'ADD', payload: expenseData });
+  };
+
+  const deleteExpense = (id) => {
+    dispatch({ type: 'DELETE', payload: id });
+  };
+
+  const updateExpense = (id, expenseData) => {
+    dispatch({ type: 'UPDATE', payload: { id: id, data: expenseData } });
+  };
+
+  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+};
+
+export default ExpensesContextProvider;
