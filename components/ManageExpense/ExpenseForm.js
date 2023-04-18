ef07639b5981117d54729288d1f930d8a3d9c45b
Your Name <urielparavi75@gmail.com>
2023-04-18 21:26:59 +0300

    156

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import Input from './Input';
 import { useState } from 'react';
 import Button from '../UI/Button';
@@ -31,6 +31,15 @@ const ExpenseForm = ({
       date: new Date(inputValues.date),
       description: inputValues.description,
     };
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      Alert.alert('Invalid input', 'Please check your input values');
+      return;
+    }
 
     onSubmit(expenseData);
   };
